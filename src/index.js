c18329da7802e4ddf3028ca5800569e629007510
Nistler <evgensin@gmail.com>
2020-09-11 00:30:38 +0300

    git pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import TodoBox from './App.jsx';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TodoBox from "./App.jsx";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoBox />
   </React.StrictMode>,
-  document.getElementById('tasksContent')
+  document.getElementById("tasksContent")
 );
 
 serviceWorker.unregister();
