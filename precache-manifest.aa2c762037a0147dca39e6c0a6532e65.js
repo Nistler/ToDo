cb2f72865ee884d1c03e1eb32957282253ffd701
Nistler <evgensin@gmail.com>
2020-09-11 00:28:36 +0300

    Updates

---
+++ precache-manifest.aa2c762037a0147dca39e6c0a6532e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4378db5eae805c2f250f64dc524cbc05",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "57c1d6eeac38c678cbde",
+    "url": "/ToDo/static/css/main.78ace550.chunk.css"
+  },
+  {
+    "revision": "7746cc19044c5bbad7b4",
+    "url": "/ToDo/static/js/2.a6015519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/static/js/2.a6015519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c1d6eeac38c678cbde",
+    "url": "/ToDo/static/js/main.b530e791.chunk.js"
+  },
+  {
+    "revision": "a64d1f8367d1550a5ad8",
+    "url": "/ToDo/static/js/runtime-main.e935b743.js"
+  },
+  {
+    "revision": "d585faa750f80b69ff821ae350b7ddb1",
+    "url": "/ToDo/static/media/chikaDance.d585faa7.gif"
+  }
+]);
